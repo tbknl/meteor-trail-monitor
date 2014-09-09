6ff4c1b7e588e19649ea9a227e738ab2fc4d1ecf
Dave van Soest <dave.soest@gfk.com>
2014-09-09 15:53:14 +0200

    Ready for new package system (Meteor 0.9).

--- package.js
+++ package.js
@@ -1,9 +1,14 @@
 Package.describe({
-    summary: "Trail-Monitor - Client-side monitoring for Trail log messages"
+    summary: "Trail-Monitor - Client-side monitoring for Trail log messages",
+    version: "0.2.1",
+    name: 'mrt:trail-monitor',
+    git: "https://github.com/tbknl/meteor-trail-monitor.git"
 });
 
-Package.on_use(function(api) {
-    api.use('trail', ['server', 'client']);
+Package.onUse(function(api) {
+    api.versionsFrom('0.9.0');
+
+    api.use('mrt:trail@0.1.1', ['server', 'client']);
     api.use('deps', 'client');
     api.use('templating', 'client');
     api.use('session', 'client');
@@ -11,19 +16,19 @@ Package.on_use(function(api) {
     // Allow us to detect 'insecure'.
     api.use('insecure', {weak: true});
 
-    api.add_files('trail_monitor.js', ['server', 'client']);
-    api.add_files('trail_monitor-server.js', ['server']);
-    api.add_files('trail_monitor-client.js', ['client']);
+    api.addFiles('trail_monitor.js', ['server', 'client']);
+    api.addFiles('trail_monitor-server.js', ['server']);
+    api.addFiles('trail_monitor-client.js', ['client']);
 
-    api.add_files('monitor_console/trail_monitor_console.html', ['client']);
-    api.add_files('monitor_console/trail_monitor_console.js', ['client']);
-    api.add_files('monitor_console/trail_monitor_console.css', ['client']);
+    api.addFiles('monitor_console/trail_monitor_console.html', ['client']);
+    api.addFiles('monitor_console/trail_monitor_console.js', ['client']);
+    api.addFiles('monitor_console/trail_monitor_console.css', ['client']);
 
     api.export && api.export('Trail', ['server', 'client']);
 });
 
-Package.on_test(function (api) {
-  api.use(['trail', 'tinytest', 'test-helpers']);
+Package.onTest(function (api) {
+    api.use(['mrt:trail@0.1.1', 'tinytest', 'test-helpers']);
 
-  api.add_files('trail_monitor_tests.js', ['server']);
+    api.add_files('trail_monitor_tests.js', ['server']);
 });
